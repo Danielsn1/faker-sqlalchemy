ad6c48f1ddab6c147fe9e62d0c08d4b428b43c9b
Nathan Daniels <nathan.daniels@growersedge.com>
2023-08-23 16:40:29 -0500

    added conditions for pandas implimentation

--- tests/test_provider.py
+++ tests/test_provider.py
@@ -4,7 +4,7 @@ from typing import Union
 from faker import Faker
 
 from faker_sqlalchemy import SqlAlchemyProvider
-from tests.test_models import Model, RelationshipModel
+from tests.test_models import Model, RelationshipModel, model
 
 
 class SqlAlchemyProviderTests(unittest.TestCase):
@@ -18,16 +18,28 @@ class SqlAlchemyProviderTests(unittest.TestCase):
 
     def test_primary_key_fields_are_not_generated_by_default(self):
         result = self.faker.sqlalchemy_model(Model)
+        table_result = self.faker.pandas_series(model)
+        model_result = self.faker.pandas_series(Model)
         self.assertIsNone(result.id)
+        self.assertIsNone(table_result.id)
+        self.assertIsNone(model_result.id)
 
     def test_primary_key_fields_may_be_generated(self):
         result = self.faker.sqlalchemy_model(Model, generate_primary_keys=True)
+        table_result = self.faker.pandas_series(model)
+        model_result = self.faker.pandas_series(Model)
         self.assertIsNotNone(result.id)
+        self.assertIsNotNone(table_result.id)
+        self.assertIsNotNone(model_result.id)
 
     def test_fields_may_be_overridden(self):
         override = self.faker.pystr()
         result = self.faker.sqlalchemy_model(Model, string=override)
+        table_result = self.faker.pandas_series(model, string=override)
+        model_result = self.faker.pandas_series(Model, string=override)
         self.assertEqual(result.string, override)
+        self.assertEqual(table_result.string, override)
+        self.assertEqual(model_result.string, override)
 
     def test_relationships_are_set_to_none_by_default(self):
         result = self.faker.sqlalchemy_model(RelationshipModel)
